def21da7de84d0ed6580dceec1a5cf7b73f3d6d2
Kevin-amo <LQR0430@outlook.com>
2026-04-15 21:51:18 +0800

    接口文档bug修复

--- src/main/java/blog/config/WebMvcConfig.java
+++ src/main/java/blog/config/WebMvcConfig.java
@@ -4,7 +4,6 @@ import blog.intercept.LoginInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -26,19 +25,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/doc.html")
                 .excludePathPatterns("/doc.html/**")
                 .excludePathPatterns(
+                        "/v3/api-docs",
                         "/v3/api-docs/**",
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
                         "/swagger-resources/**",
                         "/webjars/**",
                         "/error",
                         "/favicon.ico"
                 );
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("doc.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
--- src/main/java/blog/mapper/CommentMapper.java
+++ src/main/java/blog/mapper/CommentMapper.java
@@ -16,13 +16,12 @@ public interface CommentMapper {
      * @param comment 评论内容
      */
     @Insert("INSERT INTO comment(article_id, parent_id, nickname, content, status, is_deleted, create_time) VALUES " +
-            "(#{articleId}, #{parentId}, #{niclname}, #{content}, #{status}, #{isDeleted}, #{createTime})")
+            "(#{articleId}, #{parentId}, #{nickname}, #{content}, #{status}, #{isDeleted}, #{createTime})")
     int insertComment(Comment comment);
 
     /**
      * 通过文章id查找
      * @param articleId 文章id
-     * @return
      */
     @Select("SELECT id, article_id AS articleId, parent_id AS parentId, nickname, content, status, is_deleted, create_time AS createTime" +
             " FROM comment WHERE article_id = #{articleId} AND is_deleted = 0 AND status = 1 ORDER BY create_time DESC")
